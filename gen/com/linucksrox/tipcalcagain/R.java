57a4ab9d84c5efd192d1c16a9c041e032981e875
linucksrox <ericdaly.aplus@gmail.com>
2014-06-04 23:05:54 -0400

    added percent label, amount, and seekbar which includes a proper listener

--- gen/com/linucksrox/tipcalcagain/R.java
+++ gen/com/linucksrox/tipcalcagain/R.java
@@ -1094,6 +1094,8 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int billAmountEditText=0x7f05003e;
+        public static final int billAmountLabel=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1116,6 +1118,9 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int percentAmount=0x7f050040;
+        public static final int percentLabel=0x7f05003f;
+        public static final int percentSeekBar=0x7f050041;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1193,8 +1198,7 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_t=0x7f030018;
-        public static final int fragment_t=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1237,9 +1241,10 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int bill_amount_label=0x7f0a000e;
+        public static final int percent_amount=0x7f0a0010;
+        public static final int percent_label=0x7f0a000f;
     }
     public static final class style {
         /** 
--- src/com/linucksrox/tipcalcagain/TipCalcAgainActivity.java
+++ src/com/linucksrox/tipcalcagain/TipCalcAgainActivity.java
@@ -1,14 +1,86 @@
 package com.linucksrox.tipcalcagain;
 
+import java.text.NumberFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class TipCalcAgainActivity extends Activity {
+public class TipCalcAgainActivity extends Activity
+	implements OnEditorActionListener, OnSeekBarChangeListener {
+	
+	private String billAmountString;
+	private float billAmount;
+	private EditText billAmountEditText;
+	private TextView percentAmountTextView;
+	private SeekBar percentSeekBar;
+	private float tipPercent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_t);
+		
+		// get a handle on the widgets we need to get input from or change
+		billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+		percentAmountTextView = (TextView) findViewById(R.id.percentAmount);
+		percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+		
+		// set up listeners for stuff
+		billAmountEditText.setOnEditorActionListener(this);
+		percentSeekBar.setOnSeekBarChangeListener(this);
+	}
+	
+	public void calculateAndDisplay() {
+		// get the bill amount, but if nothing was entered then use $0
+		billAmountString = billAmountEditText.getText().toString();
+		if (billAmountString.equals("")) {
+			billAmount = 0;
+		}
+		else {
+			billAmount = Float.parseFloat(billAmountString);
+		}
+		
+		// get tip percent
+		int progress = percentSeekBar.getProgress();
+		tipPercent = (float) progress / 100;
+		
+		// display new values
+		NumberFormat percent = NumberFormat.getPercentInstance();
+		percentAmountTextView.setText(percent.format(tipPercent));
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+			calculateAndDisplay();
+		}
+		return false;
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		calculateAndDisplay();
 	}
 
 }
