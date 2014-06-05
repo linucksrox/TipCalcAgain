330231a238bbd4fb188964ac43af81152b26c3c7
linucksrox <ericdaly.aplus@gmail.com>
2014-06-04 21:09:32 -0400

    initial commit, blank android app

---
+++ gen/com/linucksrox/tipcalcagain/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.linucksrox.tipcalcagain;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/linucksrox/tipcalcagain/TipCalcAgainActivity.java
@@ -0,0 +1,14 @@
+package com.linucksrox.tipcalcagain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TipCalcAgainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_t);
+	}
+
+}
